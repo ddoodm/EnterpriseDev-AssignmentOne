36ddb71ba553755f11127be0c1264e1943d2ae61
Henry Saal <henrico789@gmail.com>
2016-04-03 18:18:57 +1000

    commit test + accountant stub

---
+++ ENETCare-IMS/ENETCare-IMS/Users/Accountant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENETCare.IMS.Users
+{
+    public class Accountant : User
+    {
+        public Accountant(
+            string name,
+            string username,
+            string plaintextPassword)
+            : base (name,username,plaintextPassword)
+        {
+
+        }
+    }
+}
