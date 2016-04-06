18c65decef938257edcd5613bb06ce8c15b765d9
Daum Park <claumpark@gmail.com>
2016-04-05 23:58:44 -0700

    Manager creation unit test

---
+++ ENETCare-IMS/ENETCare-IMS-Tests/UsersTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ENETCare.IMS;
+using ENETCare.IMS.Interventions;
+using ENETCare.IMS.Users;
+
+namespace ENETCare.IMS.Tests
+{
+    [TestClass]
+    public class UsersTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            Districts.PopulateDistricts();
+        }
+
+        [TestMethod]
+        public void Check_Manager_District()
+        {
+            Manager testManager = new Manager("Bob Bobson", "bobson.bob", "dCmEp_98T65",
+                Districts.GetDistrictByID(1), 50, 50);
+
+            Assert.IsTrue(testManager.District.Name == "Rural Indonesia");
+        }
+    }
+}
