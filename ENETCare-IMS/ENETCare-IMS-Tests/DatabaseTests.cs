3bc56b4c995cc90581a1ef924ec1660bbe901a49
Deinyon Davies <deinyond@gmail.com>
2016-04-22 17:13:10 +1000

    Database connection test method succeeds.

---
+++ ENETCare-IMS/ENETCare-IMS-Tests/DatabaseTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ENETCare.IMS.Tests
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        /// <summary>
+        /// Sets the path of the Data directory for
+        /// interfacing with the database.
+        /// </summary>
+        /// <param name="context"></param>
+        [AssemblyInitialize]
+        public static void SetupDataDirectory(TestContext context)
+        {
+            string path = Path.GetFullPath(Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                @"..\..\..\ENETCare-IMS-Data"));
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
+
+        [TestMethod]
+        public void Connection_OpenClose_Success()
+        {
+            string connectionString = ConfigurationManager
+                .ConnectionStrings["DatabaseConnection"]
+                .ConnectionString;
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            conn.Close();
+        }
+    }
+}
