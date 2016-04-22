f6b3941531ce88cfa4ff3ab6f79ce8e7727d861a
Deinyon Davies <deinyond@gmail.com>
2016-04-22 16:16:16 +1000

    Initial commit of Service Based Database including Districts and Clients tables.
    Database exists in a new project. Web.conf variable "DataDirectory" is configured for your local machine by a method in the Web project's Startup class.
    
    Also installed DbUp into the Data project, though it does not function properly yet.
    
    Please try attaching the MDF database on your own machine to see if it actually works.

--- ENETCare-IMS/ENETCare-IMS-WebApp/Startup.cs
+++ ENETCare-IMS/ENETCare-IMS-WebApp/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using System.IO;
+
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ENETCare_IMS_WebApp.Startup))]
@@ -6,7 +9,20 @@ namespace ENETCare_IMS_WebApp
 {
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
+            ConfigureDataDirectoryPath();
             ConfigureAuth(app);
+        }
+
+        /// <summary>
+        /// Sets the path of "DataDirectory" to the
+        /// absolute path of the data directory
+        /// within the Data project.
+        /// </summary>
+        private void ConfigureDataDirectoryPath()
+        {
+            string path = Path.GetFullPath(Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, @"..\ENETCare-IMS-Data"));
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
         }
     }
 }
